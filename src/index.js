30e0075980632f3f3003446ced1ca045f1c98816
Muhammad Riyead Rahman <helloworld.asset@gmail.com>
2022-06-26 10:49:31 +0600

    complete

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+// import React from 'react';
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const todoTitte = "Call Family";
-const todoDesc = "Lorem, ipsum dolor sit amet consectetur adipisicing elit"
+const todoDesc = "Lorem, ipsum dolor sit amet consectetur adipisicing elit";
 const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
 const currentYear = date.getFullYear();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <h1>Todo App</h1>
-  <h3>{todoTitte}</h3>
-  <p>{todoDesc}</p>
-  <p>{dateName + "/" + monthName + "/" + currentYear}</p>
+    <h1 className='headingStyle largeText'>Todo App</h1>
+    <h3>{todoTitte}</h3>
+    <p>{todoDesc}</p>
+    <p>{dateName + "/" + monthName + "/" + currentYear}</p>
   </div>
 );
-
-
