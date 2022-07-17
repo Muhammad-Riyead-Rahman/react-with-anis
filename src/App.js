20c44e100edb185929ecd52d394cb37e5ac506c5
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2022-07-17 12:22:15 +0600

    useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Message from './components/Message';
 const App = () => {
   const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(false);
   console.log("app rendering");
+
+  const handleIncrementMessage = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+
   return (
     <div>
+      {toggle ? "on" : "off"}
+      <button onClick={() => {setToggle(!toggle)}}>Toggle</button>
       <h1>Count: {count}</h1>
       <button onClick={() => { setCount((count) => count + 1) }}>Increment</button>
       
-      <Message numberOfMessage={count} />
+      <Message numberOfMessage={count} onHandleIncrement={handleIncrementMessage} />
     </div>
   )
 }
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,11 @@
 import React, { memo } from 'react'
 
-const Message = ({numberOfMessage}) => {
+const Message = ({numberOfMessage, onHandleIncrement}) => {
   console.log("Message Rendering")
   return (
     <div>
       <p>send {numberOfMessage} messages</p>
+      <button onClick={onHandleIncrement}>Increment Message Number</button>
     </div>
   )
 }
