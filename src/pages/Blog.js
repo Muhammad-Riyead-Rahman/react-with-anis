f66abb4616e01908efa807111837efed00780ed7
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2022-07-17 09:05:52 +0600

    useLocation Hook

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,22 +1,27 @@
-import React, {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
-import { blogsData } from '../data';
+import { useLocation, useParams } from 'react-router-dom';
+
 
 const Blog = () => {
   const { title } = useParams();
-  const [bodyData, setBodyData] = useState("");
+  const location = useLocation();
+  console.log(location);
+
+  /* const [bodyData, setBodyData] = useState("");
 
   useEffect(() => {
     const blogData = blogsData.filter((blog) => blog.title === title);
     setBodyData(blogData[0].body)
-  }, [])
+  }, []) */
 
 
   return (
     <div>
       <h1>{title}</h1>
-      <p>{bodyData.slice(0, 500)}</p>
-      <p>{bodyData.slice(501, 5000)}</p>
+      {/* <p>{bodyData.slice(0, 500)}</p>
+      <p>{bodyData.slice(501, 5000)}</p> */}
+
+      <p>{location.state.body.slice(0, 500)}</p>
+      <p>{location.state.body.slice(500, 5000)}</p>
     </div>
   )
 }
