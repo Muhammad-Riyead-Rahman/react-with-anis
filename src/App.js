0e7f862fe1e8185ad7f30d7e1abbccd030299e3a
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2022-07-17 14:14:25 +0600

    mapping

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import Card from "./components/card";
 import "./index.css";
+import data from "./data.json";
 
 function App() {
+
+  console.log(data[0].title);
+
+  let items = [];
+
+  /* // using for loop
+  for (let x = 0; x < data.length; x++) {
+    items.push(<Card titleText={data[x].title} descText={data[x].desc} />)
+  } */
+
+  items = data.map((item, index) => <Card key={index} titleText={item.title} descText={item.desc} />)
+
   return <div>
     <h1 className='headingStyle largeText'>Todo App</h1>
-    <Card titleText="Call Mother" descText="this is desc1"/>
-    <Card titleText="Call Father" descText="this is desc2" />
-    <Card titleText="Call Sister" descText="this is desc3"/>
-    <Card titleText="call Home" descText="this is desc4"/>
+
+
+    {/* <Card titleText={data[0].title} descText={data[0].desc} />
+    <Card titleText={data[1].title} descText={data[1].desc} />
+    <Card titleText={data[2].title} descText={data[2].desc} />
+    <Card titleText={data[3].title} descText={data[3].desc} /> */}
+
+    {items}
+    
   </div>
 }
 
