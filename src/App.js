6b562c1a2ee83713795c98bd09e0c12dfa10eb37
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2022-07-17 13:53:17 +0600

    props and destructuring

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import "./index.css";
 function App() {
   return <div>
     <h1 className='headingStyle largeText'>Todo App</h1>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card titleText="Call Mother" descText="this is desc1"/>
+    <Card titleText="Call Father" descText="this is desc2" />
+    <Card titleText="Call Sister" descText="this is desc3"/>
+    <Card titleText="call Home" descText="this is desc4"/>
   </div>
 }
 
--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,17 @@
-const todoTitte = "Call Family";
-const todoDesc = "Lorem, ipsum dolor sit amet consectetur adipisicing elit";
+/* const todoTitte = "Call Family";
+const todoDesc = "Lorem, ipsum dolor sit amet consectetur adipisicing elit"; */
+
 const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
 const currentYear = date.getFullYear();
 
-function Card() {
+function Card(props) {
+  const { titleText, descText } = props;
   return (
     <div className='card'>
-      <h3 className='cardTitle'>{todoTitte}</h3>
-      <p className='cardDesc'>{todoDesc}</p>
+      <h3 className='cardTitle'>{titleText}</h3>
+      <p className='cardDesc'>{descText}</p>
       <p className='cardFooter'>
         {dateName + "/" + monthName + "/" + currentYear}
       </p>
