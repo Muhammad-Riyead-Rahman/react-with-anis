a5f10fc012eee7707023b753d2eecdbb1f76e635
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2022-07-17 12:33:23 +0600

    useMemo hook

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,22 @@
-import React, { memo } from 'react'
+import React, { memo, useMemo } from 'react'
 
 const Message = ({numberOfMessage, onHandleIncrement}) => {
-  console.log("Message Rendering")
+  console.log("Message Rendering");
+
+  const calculatedNumber = useMemo(
+  
+    () => {
+      let number = 0;
+      for (let index = 0; index < 5000000000; index++) {
+        number++;
+      }
+      return number;
+    }, []);
+
+
   return (
     <div>
+      <h2>Number: {calculatedNumber}</h2>
       <p>send {numberOfMessage} messages</p>
       <button onClick={onHandleIncrement}>Increment Message Number</button>
     </div>
